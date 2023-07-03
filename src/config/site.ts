c6fbd7e67d1810c2117f5b70df0c714471923f92
Coding SA <codingwebsa@gmail.com>
2023-07-03 18:13:45 +0600

    init

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export type siteConfigType = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Knockbot",
+  shortName: "Kn",
+  description: "Explore The whole new Era of AI Power Life.",
+};
---
+++ src/lib/fonts.ts
@@ -0,0 +1,6 @@
+import localFont from "next/font/local";
+
+export const cabinetFont = localFont({
+  src: "../../public/fonts/cabinet.ttf",
+  variable: "--font-cabinet",
+});
